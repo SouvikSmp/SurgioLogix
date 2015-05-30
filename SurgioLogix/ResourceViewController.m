00dcdd059aeb94ae2eee09020b4e2943d52f6e1c
Sayanta <Karmick@SayantaMacMini.local>
2015-05-30 16:19:58 +0530

    commit

--- SurgioLogix/ResourceViewController.m
+++ SurgioLogix/ResourceViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ResourceViewController.h"
+#import "WebsiteViewController.h"
 
 //#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0];
 
@@ -27,6 +28,18 @@
 {
     
 }
+- (IBAction)btnActionWebsite:(id)sender
+{
+    WebsiteViewController *resource;
+    if(IS_IPAD)
+        resource=[IPADSTORY instantiateViewControllerWithIdentifier:Website];
+    else
+        resource=[IPHONESTORY instantiateViewControllerWithIdentifier:Website];
+    
+    
+    [self PushViewController:resource TransitationFrom:kCATransitionFade];
+}
+
 - (IBAction)backToPrevious:(id)sender {
     [self.navigationController popToRootViewControllerAnimated:YES];
 }
